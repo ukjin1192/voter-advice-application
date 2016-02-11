f6c78de450dcc7732bdb7e5d6dd6774b5019917c
Uk-Jin, Yang <ukjin1192@gmail.com>
2016-02-11 21:26:08 +0900

    Send email when voice of customer created

--- survey/apps/main/signals.py
+++ survey/apps/main/signals.py
@@ -1,6 +1,8 @@
 #!usr/bin/python
 # -*- coding: utf-8 -*-
 
+from django.conf import settings
+from django.core.mail import send_mail
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from main.models import User, Party, Question, Choice, Answer, Result, RotationMatrix, VoiceOfCustomer
@@ -14,3 +16,17 @@ def update_cache_when_rotation_matrix_deployed(sender, instance, created, **kwar
     """
     if created == False and instance.is_deployed == True:
         redis.set_rotation_matrix_cache()
+
+
+@receiver(post_save, sender=VoiceOfCustomer)
+def send_mail_when_voice_of_customer_created(sender, instance, created, **kwargs):
+    """
+    Send mail when voice of customer created
+    """
+    if created == True:
+        smtp_address = getattr(settings, 'EMAIL_HOST_USER')
+        send_mail('[VOC] VOC created', 
+                instance.context, 
+                smtp_address, 
+                [smtp_address, ], 
+                fail_silently=False)
